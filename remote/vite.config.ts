1b92c07e9fae8a74e569aa171196ae21f147c805
Anton Trafimovich <tonytrof@gmail.com>
2026-02-06 17:23:41 +0100

    Added working mf solution.

--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import { federation } from '@module-federation/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [
     react(),
     federation({
@@ -25,4 +26,4 @@ export default defineConfig({
   build: {
     target: 'chrome89'
   }
-});
+}));
